86caf4b0d152e5cf8aab94826bdc4540fb27e0f6
DinithShamika <dinithsh256@gmail.com>
2025-08-12 14:34:04 +0530

    Create dashboard.blade.php

---
+++ inance-construction/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1 class="text-2xl font-bold mb-4">Welcome to Admin Dashboard</h1>
+
+    <!-- You can add your admin widgets/cards here -->
+@endsection

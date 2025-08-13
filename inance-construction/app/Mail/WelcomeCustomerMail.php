94ec4f9022ac5496474c9fd587f85cc3764288f0
DinithShamika <dinithsh256@gmail.com>
2025-08-13 14:41:05 +0530

    Create WelcomeCustomerMail.php

---
+++ inance-construction/app/Mail/WelcomeCustomerMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class WelcomeCustomerMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Welcome to Inance Construction!',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.welcome-customer',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}

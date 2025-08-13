57187c540193a866fccee6c7da5172c3a822b46f
DinithShamika <dinithsh256@gmail.com>
2025-08-13 14:41:08 +0530

    Update ContactMessage.php

--- inance-construction/app/Models/ContactMessage.php
+++ inance-construction/app/Models/ContactMessage.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ContactMessage extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email', 
+        'project_type',
+        'message',
+        'user_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

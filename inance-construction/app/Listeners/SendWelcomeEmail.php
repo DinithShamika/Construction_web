e5f079ee7fcedc39655870353eec040de0a9165c
DinithShamika <dinithsh256@gmail.com>
2025-08-13 14:41:07 +0530

    Create SendWelcomeEmail.php

---
+++ inance-construction/app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeCustomerMail;
+
+class SendWelcomeEmail
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(Registered $event): void
+    {
+        try {
+            Mail::to($event->user->email)->send(new WelcomeCustomerMail($event->user));
+        } catch (\Exception $e) {
+            // Email will be logged, registration continues
+        }
+    }
+}
